e4f21e641ec5bc5688c54ac42bfa4a9be402dfe2
Dinesh Bobburu <bobburudinesh123@gmail.com>
2025-05-25 16:19:17 -0500

    Handled event grouping by waiting for 2 events to happen to resume a particular task

--- FreeRTOS/FreeRTOSConfig.h
+++ FreeRTOS/FreeRTOSConfig.h
@@ -87,6 +87,9 @@ to exclude the API function. */
 #define INCLUDE_xTaskGetIdleTaskHandle	1
 #define INCLUDE_pxTaskGetStackStart		1
 
+/*Event Groups related Flags*/
+#define configSUPPORT_DYNAMIC_ALLOCATION 	1
+
 /* Cortex-M specific definitions. */
 #ifdef __NVIC_PRIO_BITS
 	/* __BVIC_PRIO_BITS will be specified when CMSIS is being used. */
